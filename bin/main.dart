bc7642a843c2464e2e082438c092a34804aa629e
YkaroMateus <103946638+YkaroMateus@users.noreply.github.com>
2022-07-28 15:54:58 -0300

    fix: add middleware Middleware Interception

--- bin/main.dart
+++ bin/main.dart
@@ -2,12 +2,16 @@ import 'package:shelf/shelf.dart';
 
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
+import 'infrastructure/middleware_interception.dart';
 import 'infrastructure/server_custom.dart';
 import 'services/news_service.dart';
 
 void main() async {
   var cascateHandler = Cascade().add(LoginApi().handler).add(BlogApi(NewsService()).handler).handler;
-  var handler = Pipeline().addMiddleware(logRequests()).addHandler(cascateHandler);
+  var handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(MiddlewareInterception().middleware)
+      .addHandler(cascateHandler);
 
   await ServerCustom().initialize(
     handler: handler,
